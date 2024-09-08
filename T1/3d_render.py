0dda3a266c3813fbe3d15fdb99a6a4dc670fc714
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-09-08 19:18:52 -0300

    T1 - Cube & Sphere Independent movement

--- T1/3d_render.py
+++ T1/3d_render.py
@@ -7,6 +7,10 @@ from OpenGL.GLU import *
 
 display = (800, 600)
 
+# Initial cube and sphere positions
+cube_pos = [-2, 0, 0]
+sphere_pos = [2, 0, 0]
+
 
 def init():
     pygame.init()
@@ -67,17 +71,54 @@ def draw_sphere(center_pos, color):
     glPopMatrix()
 
 
+def move_cube(cube_pos):
+    """
+    Moves the cube using WASD keys.
+    Updates the cube's position in the cube_pos list.
+    """
+    keys = pygame.key.get_pressed()
+    if keys[K_w]:
+        cube_pos[1] += 0.1  # Move up
+    if keys[K_s]:
+        cube_pos[1] -= 0.1  # Move down
+    if keys[K_a]:
+        cube_pos[0] -= 0.1  # Move left
+    if keys[K_d]:
+        cube_pos[0] += 0.1  # Move right
+
+
+def move_sphere(sphere_pos):
+    """
+    Moves the sphere using arrow keys.
+    Updates the sphere's position in the sphere_pos list.
+    """
+    keys = pygame.key.get_pressed()
+    if keys[K_UP]:
+        sphere_pos[1] += 0.1  # Move up
+    if keys[K_DOWN]:
+        sphere_pos[1] -= 0.1  # Move down
+    if keys[K_LEFT]:
+        sphere_pos[0] -= 0.1  # Move left
+    if keys[K_RIGHT]:
+        sphere_pos[0] += 0.1  # Move right
+
+
 def main():
     init()
+    global cube_pos, sphere_pos
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            elif event.type == pygame.KEYDOWN:
+                move_cube(cube_pos)
+                move_sphere(sphere_pos)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        draw_cube((-2, 0, 0), (1, 0, 0))
-        draw_sphere((2, 0, 0), (0, 0, 1))
+        draw_cube(cube_pos, (1, 0, 0))
+        draw_sphere(sphere_pos, (0, 0, 1))
+
         glRotatef(1, 3, 1, 1)
         
         pygame.time.wait(10)
